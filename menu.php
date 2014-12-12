8c61c6d0c4fb49333bb3f26e60b63dcdb891bb58
root <root@localhost.localdomain>
2014-12-12 22:30:13 +0000

    tool box and icon

--- menu.php
+++ menu.php
@@ -4,30 +4,14 @@
                                 <br><br><br><br><br><br>
                                 <h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
                                 <ul class="nav nav-stacked">
-                                        <li><a href=""><i class="glyphicon glyphicon-flash"></i> Alerts</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-link"></i> Links</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-book"></i> Books</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-briefcase"></i> Tools</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-time"></i> Real-time</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-plus"></i> Advanced..</a></li>
+                                        <li><a href=""><i class="glyphicon glyphicon-search"></i> Find learnifyer</a></li>
+                                        <li><a href=""><i class="glyphicon glyphicon-plus"></i> Create learnifyer</a></li>
+                                        <li><a href=""><i class="glyphicon glyphicon-star-empty"></i> My best learnifyer </a></li>
+                                        <li><a href=""><i class="glyphicon glyphicon-cog"></i> Settings</a></li>
                         </ul>      
                 </div>
                         <div class="col-md-8 column">
                         </div>
-                        <div class="col-md-2 column">
-                                <br><br><br><br><br><br>
-                                <h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
-                                <ul class="nav nav-stacked">
-                                        <li><a href=""><i class="glyphicon glyphicon-flash"></i> Alerts</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-link"></i> Links</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-book"></i> Books</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-briefcase"></i> Tools</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-time"></i> Real-time</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-plus"></i> Advanced..</a></li>
-                        </ul>      
-                </div>
                         <div class="col-md-4s column">
                         </div>
                         <div class="col-md-4 column">
