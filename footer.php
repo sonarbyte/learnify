4900a8eeef629e07854537274bc52ace0d672ac9
Qlimax <root@localhost.localdomain>
2014-12-12 22:07:16 +0100

    add fonts

---
+++ footer.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default navbar-fixed-bottom hidden-xs">
+  <div class="container">
+    <div class="navbar-collapse collapse">
+      <a class="navbar-brand" href=""><i><b>Ashdworf</b></i></a>
+    </div>
+  </div>
+</nav>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
