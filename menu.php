b21545182e7e7ae37dd146db1d7bea949ae68af7
Qlimax <root@localhost.localdomain>
2014-12-13 01:28:30 +0100

    fix layout

--- menu.php
+++ menu.php
@@ -1,20 +1,8 @@
- <div class="row">
-                <div class="container">
-                        <div class="col-md-2 column">
-                                <br><br><br><br><br><br>
-                                <h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
-                                <ul class="nav nav-stacked">
-                                        <li><a href="./?body=recherche.php"><i class="glyphicon glyphicon-search"></i> Find learnifyer</a></li>
-                                        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-plus"></i> Create learnifyer</a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-star-empty"></i> My best learnifyer </a></li>
-                                        <li><a href=""><i class="glyphicon glyphicon-cog"></i> Settings</a></li>
-                        </ul>      
-                </div>
-                        <div class="col-md-8 column">
-                        </div>
-                        <div class="col-md-4s column">
-                        </div>
-                        <div class="col-md-4 column">
-                        </div>
-                </div>
-        </div>
+<br><br><br><br><br><br>
+<h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
+<ul class="nav nav-stacked">
+        <li><a href="./?body=recherche.php"><i class="glyphicon glyphicon-search"></i> Find learnifyer</a></li>
+        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-plus"></i> Create learnifyer</a></li>
+        <li><a href=""><i class="glyphicon glyphicon-star-empty"></i> My best learnifyer </a></li>
+         <li><a href=""><i class="glyphicon glyphicon-cog"></i> Settings</a></li>
+</ul>
