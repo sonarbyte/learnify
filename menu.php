7f5e899f1802f59831fc7d8844350f9b6765935e
root <root@localhost.localdomain>
2014-12-13 16:17:44 +0000

    fix menu

--- menu.php
+++ menu.php
@@ -2,8 +2,9 @@
 <h3><i class="glyphicon glyphicon-briefcase"></i> Toolbox</h3>
 <ul class="nav nav-stacked">
         <li><a href="./?body=recherche.php"><i class="glyphicon glyphicon-search"></i> Find learnifyer</a></li>
-        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-plus"></i> Create learnifyer</a></li>
+        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-th"></i> Browse learnifyers</a></li>
+        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-plus"></i> Create learnifyers</a></li>
         <li><a href="./?body=my_learnifyer.php"><i class="glyphicon glyphicon-th-list"></i> My learnifyers</a></li>
         <li><a href="./?body=add.php"><i class="glyphicon glyphicon-star-empty"></i> Favorites </a></li>
-        <li><a href="./?body=add.php"><i class="glyphicon glyphicon-star-empty"></i>Browse learnifyer</a></li>
+
 </ul>
