5c85c18a683bdc9ecc06c0502c9b87e5e5432a83
qlimaxx <samirxallous@gmail.com>
2015-06-02 00:57:47 +0100

    update home page line 14

--- home.php
+++ home.php
@@ -5,13 +5,13 @@
 if(isset($_SESSION['dat_r']) and !empty($_SESSION['dat_r']))
 {
 $ses = $_SESSION['dat_r'] ;
-$con    = mysqli_connect("127.0.0.1", "root", "4306zodiac", "client_db");
+$con    = mysqli_connect("127.0.0.1", "root", "toor", "client_db");
     $result = mysqli_query($con, "SELECT * FROM clients where session='$ses' ;");
     $row = mysqli_fetch_array($result) ;
     echo 'Welcome'.$row['login'] ;
   mysqli_close($con);
 
-}>
+}
 else { echo "Please login " ;
 }
 ?>
